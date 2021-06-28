23f2bc5eaf6ee83ccc843e13e026fd00ddd32915
Daniel Avelino <daniel_avelino@outlook.com>
2021-06-28 19:01:31 -0300

    initialize project

---
+++ src/main/java/com/security/entities/Role.java
@@ -0,0 +1,52 @@
+package com.security.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String role;
+
+	@ManyToMany(mappedBy = "roles")
+	private Set<User> user = new HashSet<>();
+
+	public Role() {
+	}
+
+	public Role(Long id, String role) {
+		this.id = id;
+		this.role = role;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
